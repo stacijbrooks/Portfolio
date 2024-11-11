93e516d212354942c787cc04abefddc0d0bcd1b2
stacijbrooks <stacibrooksfitness@gmail.com>
2024-11-10 19:03:56 -0600

    Initial Portfolio Commit

---
+++ Models/Projects.cs
@@ -0,0 +1,8 @@
+public class Project
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImageUrl { get; set; }
+    public string GitHubUrl { get; set; }
+}
